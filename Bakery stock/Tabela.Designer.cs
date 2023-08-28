69333b2f538952d7e89e92a0921a7f1fcbbe867a
FELIPE DA SILVA ALVES <felipe.salves14@senacsp.edu.br>
2023-08-28 16:46:27 -0300

    terminado

--- Bakery stock/Tabela.Designer.cs
+++ Bakery stock/Tabela.Designer.cs
@@ -44,12 +44,10 @@
             this.proximo = new System.Windows.Forms.Button();
             this.anterior = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.nomep = new System.Windows.Forms.TextBox();
-            this.validadep = new System.Windows.Forms.TextBox();
             this.fornecedorp = new System.Windows.Forms.TextBox();
             this.quantidadep = new System.Windows.Forms.TextBox();
             this.precop = new System.Windows.Forms.TextBox();
@@ -61,36 +59,36 @@
             // quantidade
             // 
             this.quantidade.AutoSize = true;
-            this.quantidade.Location = new System.Drawing.Point(841, 59);
+            this.quantidade.Location = new System.Drawing.Point(946, 74);
             this.quantidade.Name = "quantidade";
-            this.quantidade.Size = new System.Drawing.Size(77, 16);
+            this.quantidade.Size = new System.Drawing.Size(92, 20);
             this.quantidade.TabIndex = 23;
             this.quantidade.Text = "Quantidade";
             // 
             // fornecedor
             // 
             this.fornecedor.AutoSize = true;
-            this.fornecedor.Location = new System.Drawing.Point(710, 59);
+            this.fornecedor.Location = new System.Drawing.Point(799, 74);
             this.fornecedor.Name = "fornecedor";
-            this.fornecedor.Size = new System.Drawing.Size(77, 16);
+            this.fornecedor.Size = new System.Drawing.Size(91, 20);
             this.fornecedor.TabIndex = 22;
             this.fornecedor.Text = "Fornecedor";
             // 
             // validade
             // 
             this.validade.AutoSize = true;
-            this.validade.Location = new System.Drawing.Point(583, 59);
+            this.validade.Location = new System.Drawing.Point(656, 74);
             this.validade.Name = "validade";
-            this.validade.Size = new System.Drawing.Size(62, 16);
+            this.validade.Size = new System.Drawing.Size(71, 20);
             this.validade.TabIndex = 21;
             this.validade.Text = "Validade";
             // 
             // nome
             // 
             this.nome.AutoSize = true;
-            this.nome.Location = new System.Drawing.Point(459, 59);
+            this.nome.Location = new System.Drawing.Point(516, 74);
             this.nome.Name = "nome";
-            this.nome.Size = new System.Drawing.Size(44, 16);
+            this.nome.Size = new System.Drawing.Size(51, 20);
             this.nome.TabIndex = 20;
             this.nome.Text = "Nome";
             // 
@@ -103,7 +101,8 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66666F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66666F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66666F));
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(296, 243);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(333, 304);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 16;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
@@ -122,64 +121,70 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(879, 399);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(989, 499);
             this.tableLayoutPanel1.TabIndex = 18;
             // 
             // preco
             // 
             this.preco.AutoSize = true;
-            this.preco.Location = new System.Drawing.Point(980, 59);
+            this.preco.Location = new System.Drawing.Point(1102, 74);
             this.preco.Name = "preco";
-            this.preco.Size = new System.Drawing.Size(43, 16);
+            this.preco.Size = new System.Drawing.Size(50, 20);
             this.preco.TabIndex = 24;
             this.preco.Text = "Preço";
             // 
             // nometxt
             // 
             this.nometxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(173)))), ((int)(((byte)(55)))));
-            this.nometxt.Location = new System.Drawing.Point(430, 99);
+            this.nometxt.Location = new System.Drawing.Point(484, 124);
+            this.nometxt.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.nometxt.Name = "nometxt";
-            this.nometxt.Size = new System.Drawing.Size(100, 22);
+            this.nometxt.Size = new System.Drawing.Size(112, 26);
             this.nometxt.TabIndex = 26;
             // 
             // validadetxt
             // 
             this.validadetxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(173)))), ((int)(((byte)(55)))));
-            this.validadetxt.Location = new System.Drawing.Point(559, 99);
+            this.validadetxt.Location = new System.Drawing.Point(629, 124);
+            this.validadetxt.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.validadetxt.Name = "validadetxt";
-            this.validadetxt.Size = new System.Drawing.Size(100, 22);
+            this.validadetxt.Size = new System.Drawing.Size(112, 26);
             this.validadetxt.TabIndex = 27;
             // 
             // fornecedortxt
             // 
             this.fornecedortxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(173)))), ((int)(((byte)(55)))));
-            this.fornecedortxt.Location = new System.Drawing.Point(687, 99);
+            this.fornecedortxt.Location = new System.Drawing.Point(786, 124);
+            this.fornecedortxt.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.fornecedortxt.Name = "fornecedortxt";
-            this.fornecedortxt.Size = new System.Drawing.Size(100, 22);
+            this.fornecedortxt.Size = new System.Drawing.Size(112, 26);
             this.fornecedortxt.TabIndex = 28;
             // 
             // quantidadetxt
             // 
             this.quantidadetxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(173)))), ((int)(((byte)(55)))));
-            this.quantidadetxt.Location = new System.Drawing.Point(827, 99);
+            this.quantidadetxt.Location = new System.Drawing.Point(934, 124);
+            this.quantidadetxt.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.quantidadetxt.Name = "quantidadetxt";
-            this.quantidadetxt.Size = new System.Drawing.Size(100, 22);
+            this.quantidadetxt.Size = new System.Drawing.Size(112, 26);
             this.quantidadetxt.TabIndex = 29;
             // 
             // precotxt
             // 
             this.precotxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(173)))), ((int)(((byte)(55)))));
-            this.precotxt.Location = new System.Drawing.Point(956, 99);
+            this.precotxt.Location = new System.Drawing.Point(1076, 124);
+            this.precotxt.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.precotxt.Name = "precotxt";
-            this.precotxt.Size = new System.Drawing.Size(100, 22);
+            this.precotxt.Size = new System.Drawing.Size(112, 26);
             this.precotxt.TabIndex = 30;
             // 
             // adicionar
             // 
             this.adicionar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(173)))), ((int)(((byte)(55)))));
-            this.adicionar.Location = new System.Drawing.Point(705, 148);
+            this.adicionar.Location = new System.Drawing.Point(793, 185);
+            this.adicionar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.adicionar.Name = "adicionar";
-            this.adicionar.Size = new System.Drawing.Size(75, 23);
+            this.adicionar.Size = new System.Drawing.Size(84, 29);
             this.adicionar.TabIndex = 31;
             this.adicionar.Text = "Adicionar";
             this.adicionar.UseVisualStyleBackColor = false;
@@ -188,18 +193,19 @@
             // contador
             // 
             this.contador.AutoSize = true;
-            this.contador.Location = new System.Drawing.Point(736, 217);
+            this.contador.Location = new System.Drawing.Point(828, 271);
             this.contador.Name = "contador";
-            this.contador.Size = new System.Drawing.Size(14, 16);
+            this.contador.Size = new System.Drawing.Size(18, 20);
             this.contador.TabIndex = 32;
             this.contador.Text = "1";
             // 
             // proximo
             // 
             this.proximo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(173)))), ((int)(((byte)(55)))));
-            this.proximo.Location = new System.Drawing.Point(757, 214);
+            this.proximo.Location = new System.Drawing.Point(852, 268);
+            this.proximo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.proximo.Name = "proximo";
-            this.proximo.Size = new System.Drawing.Size(75, 23);
+            this.proximo.Size = new System.Drawing.Size(84, 29);
             this.proximo.TabIndex = 33;
             this.proximo.Text = ">";
             this.proximo.UseVisualStyleBackColor = false;
@@ -208,9 +214,10 @@
             // anterior
             // 
             this.anterior.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(173)))), ((int)(((byte)(55)))));
-            this.anterior.Location = new System.Drawing.Point(655, 214);
+            this.anterior.Location = new System.Drawing.Point(737, 268);
+            this.anterior.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.anterior.Name = "anterior";
-            this.anterior.Size = new System.Drawing.Size(75, 23);
+            this.anterior.Size = new System.Drawing.Size(84, 29);
             this.anterior.TabIndex = 34;
             this.anterior.Text = "<";
             this.anterior.UseVisualStyleBackColor = false;
@@ -219,88 +226,77 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(39, 42);
+            this.label1.Location = new System.Drawing.Point(44, 52);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.Size = new System.Drawing.Size(51, 20);
             this.label1.TabIndex = 35;
             this.label1.Text = "Nome";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(39, 99);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(62, 16);
-            this.label2.TabIndex = 36;
-            this.label2.Text = "Validade";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(39, 155);
+            this.label3.Location = new System.Drawing.Point(44, 194);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(77, 16);
+            this.label3.Size = new System.Drawing.Size(91, 20);
             this.label3.TabIndex = 37;
             this.label3.Text = "Fornecedor";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(39, 205);
+            this.label4.Location = new System.Drawing.Point(44, 256);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(77, 16);
+            this.label4.Size = new System.Drawing.Size(92, 20);
             this.label4.TabIndex = 38;
             this.label4.Text = "Quantidade";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(39, 254);
+            this.label5.Location = new System.Drawing.Point(44, 318);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(43, 16);
+            this.label5.Size = new System.Drawing.Size(50, 20);
             this.label5.TabIndex = 39;
             this.label5.Text = "Preço";
             // 
             // nomep
             // 
-            this.nomep.Location = new System.Drawing.Point(16, 67);
+            this.nomep.Location = new System.Drawing.Point(18, 84);
+            this.nomep.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.nomep.Name = "nomep";
-            this.nomep.Size = new System.Drawing.Size(100, 22);
+            this.nomep.Size = new System.Drawing.Size(112, 26);
             this.nomep.TabIndex = 40;
-            // 
-            // validadep
-            // 
-            this.validadep.Location = new System.Drawing.Point(16, 123);
-            this.validadep.Name = "validadep";
-            this.validadep.Size = new System.Drawing.Size(100, 22);
-            this.validadep.TabIndex = 41;
             // 
             // fornecedorp
             // 
-            this.fornecedorp.Location = new System.Drawing.Point(16, 180);
+            this.fornecedorp.Location = new System.Drawing.Point(18, 225);
+            this.fornecedorp.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.fornecedorp.Name = "fornecedorp";
-            this.fornecedorp.Size = new System.Drawing.Size(100, 22);
+            this.fornecedorp.Size = new System.Drawing.Size(112, 26);
             this.fornecedorp.TabIndex = 42;
             // 
             // quantidadep
             // 
-            this.quantidadep.Location = new System.Drawing.Point(16, 229);
+            this.quantidadep.Location = new System.Drawing.Point(18, 286);
+            this.quantidadep.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.quantidadep.Name = "quantidadep";
-            this.quantidadep.Size = new System.Drawing.Size(100, 22);
+            this.quantidadep.Size = new System.Drawing.Size(112, 26);
             this.quantidadep.TabIndex = 43;
             // 
             // precop
             // 
-            this.precop.Location = new System.Drawing.Point(16, 273);
+            this.precop.Location = new System.Drawing.Point(18, 341);
+            this.precop.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.precop.Name = "precop";
-            this.precop.Size = new System.Drawing.Size(100, 22);
+            this.precop.Size = new System.Drawing.Size(112, 26);
             this.precop.TabIndex = 44;
             // 
             // procurar
             // 
-            this.procurar.Location = new System.Drawing.Point(26, 314);
+            this.procurar.Location = new System.Drawing.Point(29, 392);
+            this.procurar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.procurar.Name = "procurar";
-            this.procurar.Size = new System.Drawing.Size(75, 23);
+            this.procurar.Size = new System.Drawing.Size(84, 29);
             this.procurar.TabIndex = 45;
             this.procurar.Text = "Procurar";
             this.procurar.UseVisualStyleBackColor = true;
@@ -308,9 +304,10 @@
             // 
             // reset
             // 
-            this.reset.Location = new System.Drawing.Point(247, 51);
+            this.reset.Location = new System.Drawing.Point(278, 64);
+            this.reset.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.reset.Name = "reset";
-            this.reset.Size = new System.Drawing.Size(75, 23);
+            this.reset.Size = new System.Drawing.Size(84, 29);
             this.reset.TabIndex = 46;
             this.reset.Text = "Reset";
             this.reset.UseVisualStyleBackColor = true;
@@ -319,30 +316,28 @@
             // gasto
             // 
             this.gasto.AutoSize = true;
-            this.gasto.Location = new System.Drawing.Point(1131, 59);
+            this.gasto.Location = new System.Drawing.Point(1272, 74);
             this.gasto.Name = "gasto";
-            this.gasto.Size = new System.Drawing.Size(86, 16);
+            this.gasto.Size = new System.Drawing.Size(103, 20);
             this.gasto.TabIndex = 47;
             this.gasto.Text = "Você gastou:";
             // 
             // Tabela
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Highlight;
-            this.ClientSize = new System.Drawing.Size(1370, 743);
+            this.ClientSize = new System.Drawing.Size(1541, 929);
             this.Controls.Add(this.gasto);
             this.Controls.Add(this.reset);
             this.Controls.Add(this.procurar);
             this.Controls.Add(this.precop);
             this.Controls.Add(this.quantidadep);
             this.Controls.Add(this.fornecedorp);
-            this.Controls.Add(this.validadep);
             this.Controls.Add(this.nomep);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.anterior);
             this.Controls.Add(this.proximo);
@@ -359,6 +354,7 @@
             this.Controls.Add(this.validade);
             this.Controls.Add(this.nome);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Tabela";
             this.Text = "Tabela";
             this.Load += new System.EventHandler(this.Tabela_Load);
@@ -385,12 +381,10 @@
         private System.Windows.Forms.Button proximo;
         private System.Windows.Forms.Button anterior;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox nomep;
-        private System.Windows.Forms.TextBox validadep;
         private System.Windows.Forms.TextBox fornecedorp;
         private System.Windows.Forms.TextBox quantidadep;
         private System.Windows.Forms.TextBox precop;
